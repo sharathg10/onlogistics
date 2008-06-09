462eeea93b189b41f5b27dc6a1a0f9cae414498c
izimobil <izimobil@f8fca25c-ce4c-0410-88f6-6326ef8a5772>
2008-06-09 16:18:33 +0000

    added Chain::AutoAssignTo const attribute

--- lib/Objects/_Chain.php
+++ lib/Objects/_Chain.php
@@ -3,37 +3,18 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * This file is part of Onlogistics, a web based ERP and supply chain 
- * management application. 
+ * $Source: /home/cvs/codegen/codegentemplates.py,v $
  *
- * Copyright (C) 2003-2008 ATEOR
+ * Ceci est un fichier généré, NE PAS EDITER.
  *
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU Affero General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or (at your 
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public 
- * License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * PHP version 5.1.0+
- *
- * @package   Onlogistics
- * @author    ATEOR dev team <dev@ateor.com>
- * @copyright 2003-2008 ATEOR <contact@ateor.com> 
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU AGPL
- * @version   SVN: $Id$
- * @link      http://www.onlogistics.org
- * @link      http://onlogistics.googlecode.com
- * @since     File available since release 0.1.0
- * @filesource
+ * @copyright 2002-2006 ATEOR - All rights reserved
  */
 
+
+/**
+ * _Chain class
+ *
+ */
 class _Chain extends Object {
     // class constants {{{
 
@@ -42,6 +23,9 @@ class _Chain extends Object {
     const CHAIN_TYPE_MAINTENANCE = 2;
     const CHAIN_TYPE_COURSE = 3;
     const CHAIN_TYPE_HIRING = 4;
+    const AUTOASSIGN_NONE = 0;
+    const AUTOASSIGN_PRODUCTS = 1;
+    const AUTOASSIGN_MATERIALS = 2;
     const CHAIN_STATE_UNSET = 0;
     const CHAIN_STATE_CREATED = 1;
     const CHAIN_STATE_BUILT = 2;
@@ -263,6 +247,61 @@ class _Chain extends Object {
             _Chain::CHAIN_TYPE_MAINTENANCE => _("maintenance"), 
             _Chain::CHAIN_TYPE_COURSE => _("class"), 
             _Chain::CHAIN_TYPE_HIRING => _("helicopter rental")
+        );
+        asort($array);
+        return $keys?array_keys($array):$array;
+    }
+
+    // }}}
+    // AutoAssignTo const property + getter/setter/getAutoAssignToConstArray {{{
+
+    /**
+     * AutoAssignTo int property
+     *
+     * @access private
+     * @var integer
+     */
+    private $_AutoAssignTo = 0;
+
+    /**
+     * _Chain::getAutoAssignTo
+     *
+     * @access public
+     * @return integer
+     */
+    public function getAutoAssignTo() {
+        return $this->_AutoAssignTo;
+    }
+
+    /**
+     * _Chain::setAutoAssignTo
+     *
+     * @access public
+     * @param integer $value
+     * @return void
+     */
+    public function setAutoAssignTo($value) {
+        if ($value !== null) {
+            $this->_AutoAssignTo = (int)$value;
+        }
+    }
+
+    /**
+     * _Chain::getAutoAssignToConstArray
+     * Retourne un tableau associatif avec la valeur de la constante en
+     * clef et sa représentation textuelle en valeur.
+     * Si $keys vaut true, seules les clefs sont retournées
+     *
+     * @access public
+     * @static
+     * @param boolean $keys
+     * @return array
+     */
+    public static function getAutoAssignToConstArray($keys = false) {
+        $array = array(
+            _Chain::AUTOASSIGN_NONE => _("Select an item"), 
+            _Chain::AUTOASSIGN_PRODUCTS => _("Products"), 
+            _Chain::AUTOASSIGN_MATERIALS => _("Materials")
         );
         asort($array);
         return $keys?array_keys($array):$array;
@@ -929,6 +968,7 @@ class _Chain extends Object {
             'Description' => Object::TYPE_STRING,
             'DescriptionCatalog' => Object::TYPE_STRING,
             'Type' => Object::TYPE_CONST,
+            'AutoAssignTo' => Object::TYPE_CONST,
             'BarCodeType' => 'BarCodeType',
             'SiteTransition' => 'ActorSiteTransition',
             'PivotTask' => 'ChainTask',
@@ -1105,6 +1145,15 @@ class _Chain extends Object {
                 'shortlabel'   => _('Type'),
                 'usedby'       => array('searchform', 'grid', 'addedit'),
                 'required'     => true,
+                'inplace_edit' => false,
+                'add_button'   => false,
+                'section'      => ''
+            ),
+            'AutoAssignTo'=>array(
+                'label'        => _('Auto assign to'),
+                'shortlabel'   => _('Auto assign to'),
+                'usedby'       => array('addedit'),
+                'required'     => false,
                 'inplace_edit' => false,
                 'add_button'   => false,
                 'section'      => ''
